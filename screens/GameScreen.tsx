c39cd5a6911d245d2d741496084d06c7d2fcc743
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-12 11:12:26 +0100

    Game screen file created inside screens folder

---
+++ screens/GameScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function GameScreen() {
+  return (
+    <View>
+        <Text>Game Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
