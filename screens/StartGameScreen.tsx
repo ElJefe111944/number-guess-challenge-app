9328beeca0607bca02d68a948a1308733a8d4a91
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-12 11:11:04 +0100

    StartGameScreen file created inside screens folder

---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function StartGameScreen() {
+  return (
+    <View>
+        <Text>Start Game</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
