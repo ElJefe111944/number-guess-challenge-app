a13b944cae606b8b1dd8df507df30fb308c07e69
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-12 11:13:49 +0100

    Game over  screen file created inside screens folder

---
+++ screens/GameOverScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function GameOverScreen() {
+  return (
+    <View>
+        <Text>Game Over Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
