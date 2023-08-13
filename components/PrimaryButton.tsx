2308f6dd04aa7183a85944f1724f6d924288003e
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-13 12:10:31 +0100

    Visual feedback added to primary button for iOs & andriod on press

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
 interface PrimaryButtomProps {
-    children: string;
+  children: string;
 }
 
 const PrimaryButton: React.FC<PrimaryButtomProps> = ({ children }) => {
 
   return (
-    <View>
-        <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable style={({ pressed }) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer} onPress={() => { console.log('Pressed') }} android_ripple={{ color: '#57012c' }}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,5 +19,22 @@ const PrimaryButton: React.FC<PrimaryButtomProps> = ({ children }) => {
 export default PrimaryButton;
 
 const styles = StyleSheet.create({
-
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    elevation: 2,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    backgroundColor: '#8a5871',
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    backgroundColor: '#57012c',
+  }
 });
