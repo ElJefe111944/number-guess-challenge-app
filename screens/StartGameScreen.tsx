7529462156f2a5f82456bb174eba258ff7b23758
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-13 11:37:51 +0100

    Input filed configuration added

--- screens/StartGameScreen.tsx
+++ screens/StartGameScreen.tsx
@@ -4,7 +4,13 @@ import PrimaryButton from '../components/PrimaryButton';
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-        <TextInput />
+        <TextInput 
+          style={styles.numberInput} 
+          maxLength={2} 
+          keyboardType='number-pad' 
+          autoCapitalize='none' 
+          autoCorrect={false}
+        />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -23,5 +29,17 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height:2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+    textAlign: 'center',
+
+  },
+  numberInput: {
+    height: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    width: 50,
   }
 });
