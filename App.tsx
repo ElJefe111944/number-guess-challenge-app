23fc957571195fadf46cd499bcc29c223f358905
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-14 23:22:00 +0100

    Image background styling added to StartGameScreen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,22 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 // screens
 import StartGameScreen from './screens/StartGameScreen';
+
 
 export default function App() {
   return (
     <LinearGradient 
       colors={['#ddb52f','#4e0329']}
       style={styles.rootScreen}>
+        <ImageBackground 
+          source={require('./assets/images/background.png')}
+          resizeMode='cover'
+          style={styles.rootScreen}
+          imageStyle={styles.backgroundImage}
+        >
           <StartGameScreen />
+        </ImageBackground>
     </LinearGradient>
   );
 }
@@ -16,5 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.45,
   }
 });
