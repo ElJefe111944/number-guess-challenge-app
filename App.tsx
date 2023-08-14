bc7490355748a3a20501bba4ac1f51e48a194569
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-14 23:02:09 +0100

    Background colour added to app file for all screens

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 // screens
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen />
+    <View style={styles.rootScreen}>
+          <StartGameScreen />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootScreen: {
+    backgroundColor: '#ddb52f',
+    flex: 1,
+  }
 });
