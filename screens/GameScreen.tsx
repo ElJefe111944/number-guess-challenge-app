89dac705f2c0bc299377f3f11add524004851973
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-15 14:57:04 +0100

    Components and placeholder text added to GameScreen

--- screens/GameScreen.tsx
+++ screens/GameScreen.tsx
@@ -1,13 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function GameScreen() {
   return (
-    <View>
-      <Text>Game Screen</Text>
+    <View style={styles.screen}>
+      <View>
+        <Text>Opponent's Guess</Text>
+      </View>
+      <View>
+        <Text>Higher or Lower?</Text>
+        <Button title='+' />
+        <Button title='-' />
+      </View>
+      <View>
+        <Text>
+          Round Logs
+        </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 12,
 
+  }
 });
