b7a7f7218b67064094a444068abbed3d6444eeb8
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-18 23:26:15 +0100

    Card component created and children props added

---
+++ components/ui/Card.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
+import Colours from '../../constants/colours';
+
+
+interface CardProps {
+    children: React.ReactNode;
+}
+
+
+export default function Card({ children }: CardProps) {
+    return (
+        <View style={styles.inputContainer}>{children}</View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        marginTop: 36,
+        marginHorizontal: 24,
+        backgroundColor: Colours.primary500,
+        borderRadius: 8,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: { width: 0, height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+        textAlign: 'center',
+
+    },
+});
