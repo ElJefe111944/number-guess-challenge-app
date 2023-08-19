2a454f3618b31ae250928e67becf589e4355d234
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-19 22:53:05 +0100

    Style prop added to InstructionText component

--- components/ui/InstructionText.tsx
+++ components/ui/InstructionText.tsx
@@ -1,13 +1,14 @@
-import { StyleSheet, Text } from 'react-native';
+import { StyleProp, StyleSheet, Text, TextStyle } from 'react-native';
 import Colours from '../../constants/colours';
 
 interface InstructionTextProps {
     children: React.ReactNode;
+    style?: StyleProp<TextStyle>;
 }
 
-export default function Card({ children }: InstructionTextProps) {
+export default function Card({ children, style }: InstructionTextProps) {
     return (
-        <Text style={styles.inputInstructionText}>{children}</Text>
+        <Text style={[styles.inputInstructionText, style]}>{children}</Text>
     );
 }
 
