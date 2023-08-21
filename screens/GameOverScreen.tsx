ed50a696f81156c502393af909fb2a6ff53a8d14
James Farr <61662328+ElJefe111944@users.noreply.github.com>
2023-08-21 15:59:22 +0100

    Title, image and styling added to GameOverScreen

--- screens/GameOverScreen.tsx
+++ screens/GameOverScreen.tsx
@@ -1,13 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Image, View } from 'react-native';
+import Title from '../components/ui/Title'
+import Colours from '../constants/colours';
 
 export default function GameOverScreen() {
   return (
-    <View>
-        <Text>Game Over Screen</Text>
+    <View style={styles.rootContainer}>
+      <Title title='Game Over!' />
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colours.CtaBackgroundLight,
+    overflow: 'hidden',
+    margin: 'auto',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  }
 });
